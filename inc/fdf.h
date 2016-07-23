cec589a4aba4c473bdfbffc90b77acd5dd18a1b9
Lucas BAUDRANT <lbaudran@e2r7p13.42.fr>
2016-07-23 18:36:37 +0200

    parse

--- inc/fdf.h
+++ inc/fdf.h
@@ -6,7 +6,7 @@
 /*   By: aderragu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 16:34:29 by aderragu          #+#    #+#             */
-/*   Updated: 2016/06/12 16:34:32 by aderragu         ###   ########.fr       */
+/*   Updated: 2016/07/23 15:59:56 by lbaudran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 # define FDF_H
 
 #include <mlx.h>
+#include <fcntl.h>
+
+typedef struct		s_list
+{
+	int		x;
+	int		y;
+	struct s_list *next;
+	struct s_list *prev;
+}					t_list;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,17 +6,20 @@
 /*   By: aderragu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/12 16:31:04 by aderragu          #+#    #+#             */
-/*   Updated: 2016/06/12 16:31:06 by aderragu         ###   ########.fr       */
+/*   Updated: 2016/07/23 18:28:38 by lbaudran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int 		main()
+int 		main(int argc, char **argv)
 {
+	t_list	*begin;
 	void	*mlx_i;
 	void	*win;
 
+	begin = create_list(begin);
+	recup_map(&begin, argv);
 	mlx_i = mlx_init();
 	win = mlx_new_window(mlx_i, 400, 400, "test");
 	mlx_loop(mlx_i);
