cb802d038db9809d3b25988b792587635c7c09f7
Amine DERRAGUI <aderragu@e2r7p12.42.fr>
2016-07-24 19:28:13 +0200

    ajout fonction put_pixel dans une image

--- src/main.c
+++ src/main.c
@@ -22,13 +22,50 @@ int				key_func(int key, void *param)
 	return (1);
 }
 
+int				put_pixel(char *image, int x, int y, int color)
+{
+
+	image[0 + x * 4 + y * 1600] = color >> 0;
+	image[1 + x * 4 + y * 1600] = color >> 8;
+	image[2 + x * 4 + y * 1600] = color >> 16;
+	image[3 + x * 4 + y * 1600] = color >> 24;
+	return (1);
+}
+
 int 			main(int argc, char **argv)
 {
 	t_data		data;
 	t_list		*begin;
+	char		*image;
+	int			c = 0x00FFFFFF;
+	int			col = 0x00FFFF00;
+	int			l = 400;
+	int			e = 0;
+	int			x = 0;
+	int			y = 0;
 
 	data.init = mlx_init();
 	data.win = mlx_new_window(data.init, 400, 400, "test");
+	data.img = mlx_new_image(data.init, 400, 400);
+	image = mlx_get_data_addr(data.img, &c, &l, &e);
+	while (y < 400)
+	{
+		while (x < 400)
+		{
+			put_pixel(image, x, y, col);
+			x++;
+
+		}
+		y++;
+		x = 0;
+		if (y > 100 && y < 200)
+			col = 0x00FF0000;
+		if (y >= 200 && y < 300)
+			col = 0x000000FF;
+		if (y >= 300)
+			col = 0x0000FF00;
+	}
+	mlx_put_image_to_window(data.init, data.win, data.img, 1, 1);
 	mlx_key_hook(data.win, key_func, &data);
 	mlx_loop(data.init);
 	if (argc == 1)
